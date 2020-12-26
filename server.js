0af4c92867052b551179afabc7b738dd0812f5ce
Elyse Wells <cewells89@gmail.com>
2020-12-26 03:38:18 -0500

    began building out server, connection, and orm files

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const routes = require("./controllers/hamburgerController");
+const connection = require("./config/connection")
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.engine("handlebars",exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+// app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
