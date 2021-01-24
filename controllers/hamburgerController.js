cdc1e10371eb81421435d0d57d6ef8a8fc639b36
Elyse Wells <cewells89@gmail.com>
2021-01-24 01:55:14 -0500

    added database schema and seeds

---
+++ controllers/hamburgerController.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var router = express.Router();
+
+// var burger = require("../models/burger.js");
+
+//TODO: Build out view and API routes
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+--TODO: Create seed data.
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom & Swiss', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', false);
